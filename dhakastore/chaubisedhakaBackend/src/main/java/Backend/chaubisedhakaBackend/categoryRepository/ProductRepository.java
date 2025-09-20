7e5c24e3361aee02b1f5bff950f5cef9b4e82766
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-09-20 16:28:58 +0545

    sorting by keyword

--- dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/categoryRepository/ProductRepository.java
+++ dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/categoryRepository/ProductRepository.java
@@ -2,14 +2,14 @@ package Backend.chaubisedhakaBackend.categoryRepository;
 
 import Backend.chaubisedhakaBackend.model.Category;
 import Backend.chaubisedhakaBackend.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    List<Product> findByCategoryOrderByPriceAsc(Category category);
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
 
-    List<Product> findByProductNameLikeIgnoreCase(String keyword);
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
 }
