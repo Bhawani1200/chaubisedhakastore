298d78d6ed1bb6629248ce255638d50454db20b7
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-10-09 14:45:22 +0545

    setting DTOs

---
+++ dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/payload/CartItemDTO.java
@@ -0,0 +1,17 @@
+package Backend.chaubisedhakaBackend.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemDTO {
+    private Long cartItemId;
+    private CartDTO cart;
+    private ProductDTO productDTO;
+    private Integer quantity;
+    private Double discount;
+    private double productPrice;
+}
