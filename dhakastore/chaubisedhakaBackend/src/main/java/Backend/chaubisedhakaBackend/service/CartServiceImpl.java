c1a3cf740e3f3161a04e6f83d170e27084b01ac2
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-10-16 16:56:39 +0545

    slight changes

--- dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/service/CartServiceImpl.java
+++ dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/service/CartServiceImpl.java
@@ -43,9 +43,6 @@ public class CartServiceImpl implements CartService{
     @Autowired
     ModelMapper modelMapper;
 
-    @Autowired
-    private CartService cartService;
-
     @Override
     public CartDTO addProductToCart(Long productId, Integer quantity) {
         Cart cart=createCart();
@@ -158,13 +155,6 @@ public class CartServiceImpl implements CartService{
         return cartDTO;
     }
 
-    @PutMapping("/cart/products/{productId}/quantity/{operation}")
-    public ResponseEntity<CartDTO>updateCartProduct(@PathVariable Long productId,
-                                                    @PathVariable String operation){
-        CartDTO cartDTO=cartService.updateProductQuantityInCart(productId,
-                operation.equalsIgnoreCase("delete") ? -1 : 1 );
-        return new ResponseEntity<>(cartDTO, HttpStatus.OK);
-    }
 
     @Transactional
     @Override
@@ -193,14 +183,24 @@ public class CartServiceImpl implements CartService{
         CartItem cartItem=cartItemRepository.findCartItemByProductIdAndCartId(cartId,productId);
 
         if(cartItem == null){
-            throw new APIException("Product" + product.getProductName() + "is not available in the cart");
+            throw new APIException("Product " + product.getProductName() + "is not available in the cart");
         }
 
-        cartItem.setProductPrice(product.getSpecialPrice());
-        cartItem.setQuantity(product.getQuantity()+quantity);
-        cartItem.setDiscount(product.getDiscount());
-        cart.setTotalPrice(cart.getTotalPrice() +(cartItem.getProductPrice() * quantity));
-        cartRepository.save(cart);
+        int newQuantity=cartItem.getQuantity()+quantity;
+
+        if(newQuantity<0){
+            throw new APIException("The resulting quantity cannot be negative");
+        }
+
+        if(newQuantity == 0){
+            deleteProductFromCart(cartId,productId);
+        }else {
+            cartItem.setProductPrice(product.getSpecialPrice());
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+            cartItem.setDiscount(product.getDiscount());
+            cart.setTotalPrice(cart.getTotalPrice() + (cartItem.getProductPrice() * quantity));
+            cartRepository.save(cart);
+        }
 
         CartItem updatedItem=cartItemRepository.save(cartItem);
         if(updatedItem.getQuantity() == 0){
