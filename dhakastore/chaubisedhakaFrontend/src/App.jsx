588cc8e392b910d70931afa7b4f8e22a857beeda
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-10-30 17:19:14 +0545

    setting error messages

--- dhakastore/chaubisedhakaFrontend/src/App.jsx
+++ dhakastore/chaubisedhakaFrontend/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Products from "./components/Products";
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+     <Products/>
     </>
   );
 }
