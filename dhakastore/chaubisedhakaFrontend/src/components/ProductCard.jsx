b554d45e180111e9a861217187f72981b55a42a8
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-10-30 21:36:39 +0545

    working on modal

--- dhakastore/chaubisedhakaFrontend/src/components/ProductCard.jsx
+++ dhakastore/chaubisedhakaFrontend/src/components/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import ProductViewModal from "./ProductViewModal";
 
 const ProductCard = ({
   productId,
@@ -92,6 +93,12 @@ const ProductCard = ({
           </button>
         </div>
       </div>
+      <ProductViewModal
+        open={openProductViewModal}
+        setOpen={setOpenProductViewModel}
+        product={selectedViewProduct}
+        isAvailable={isAvailable}
+      />
     </div>
   );
 };
