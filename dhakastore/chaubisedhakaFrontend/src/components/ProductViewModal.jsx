363714cefb74439d852747f9191ffb11bde1cc55
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-10-31 11:01:17 +0545

    modal

--- dhakastore/chaubisedhakaFrontend/src/components/ProductViewModal.jsx
+++ dhakastore/chaubisedhakaFrontend/src/components/ProductViewModal.jsx
@@ -1,53 +1,49 @@
-import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { Button, Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react";
 import { useState } from "react";
 
-function ProductViewModal() {
-  let [isOpen, setIsOpen] = useState(true);
+function ProductViewModal({ open, setOpen, product, isAvailable }) {
+  const {
+    id,
+    productName,
+    image,
+    description,
+    quantity,
+    price,
+    discount,
+    specialPrice,
+  } = product;
 
-  function open() {
-    setIsOpen(true);
-  }
-
-  function close() {
-    setIsOpen(false);
-  }
-
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
   return (
     <>
-      <Button
-        onClick={open}
-        className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-black/30"
-      >
-        Open dialog
-      </Button>
-
       <Dialog
-        open={isOpen}
+        open={open}
         as="div"
         className="relative z-10 focus:outline-none"
         onClose={close}
         __demoMode
       >
+        <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+              className="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all md:max-w-[620px] md:min-w-[620px] w-full"
             >
               <DialogTitle
                 as="h3"
-                className="text-base/7 font-medium text-white"
+                className="text-base/7 font-medium text-slate-800"
               >
-                Payment successful
+                {productName}
               </DialogTitle>
-              <p className="mt-2 text-sm/6 text-white/50">
-                Your payment has been successfully submitted. We’ve sent you an
-                email with all of the details of your order.
-              </p>
+              <p className="mt-2 text-sm/6 text-slate-600">{description}</p>
               <div className="mt-4">
                 <Button
                   className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
-                  onClick={close}
+                  onClick={() => setOpen(false)}
                 >
                   Got it, thanks!
                 </Button>
