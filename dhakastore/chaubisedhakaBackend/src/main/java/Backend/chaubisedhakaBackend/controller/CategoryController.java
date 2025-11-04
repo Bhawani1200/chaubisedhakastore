3f7c9ffa459f35d4bbca8ea71633b344d2d6c2a9
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-04 20:55:49 +0545

    adding public to the mapping

--- dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/controller/CategoryController.java
+++ dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/controller/CategoryController.java
@@ -21,7 +21,7 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/categories")
+    @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse>getAllCategories(
             @RequestParam(name="pageNumber",defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
             @RequestParam(name="pageSize",defaultValue = AppConstants.PAGE_SIZE,required = false)Integer pageSize,
