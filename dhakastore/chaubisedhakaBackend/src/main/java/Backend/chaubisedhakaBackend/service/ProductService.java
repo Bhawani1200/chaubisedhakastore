d6a1ea7836821ad596c642f5fc7f18f93aea32d1
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-04 15:45:10 +0545

    adding category and keyword for filtering

--- dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/service/ProductService.java
+++ dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/service/ProductService.java
@@ -11,7 +11,7 @@ public interface ProductService {
 
     ProductDTO addProduct(ProductDTO product, Long categoryId);
 
-    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder, String keyword, String category);
 
     ProductResponse searchCategoryById(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
