87b3f4cfdd83126c1df8385ae43b0955062b6be9
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-05 20:26:46 +0545

    some changes

--- dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/config/WebMvcConfig.java
+++ dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/config/WebMvcConfig.java
@@ -1,11 +1,13 @@
 package Backend.chaubisedhakaBackend.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+      registry.addResourceHandler("/images/**").addResourceLocations("file:/D/chaubisedhaka/dhakastore/chaubisedhakabackend/images/");
     }
 }
