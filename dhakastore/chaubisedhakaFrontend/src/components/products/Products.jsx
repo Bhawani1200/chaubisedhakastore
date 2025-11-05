d2b765cbd1908313d84239cc5ac4d10ef8548b61
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-05 20:55:32 +0545

    organized them into related folder

---
+++ dhakastore/chaubisedhakaFrontend/src/components/products/Products.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { FaExclamationTriangle } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCategories } from "../../store/actions";
+import Filter from "./Filter";
+import useProductFilter from "../../hook/useProductFilter";
+import Loader from "../shared/Loader";
+import PaginationForProduct from "../shared/PaginationForProduct";
+import ProductCard from "../shared/ProductCard";
+
+const Products = () => {
+  const { isLoading, errorMessage } = useSelector((state) => state.errors);
+
+  const { products, categories, pagination } = useSelector(
+    (state) => state.products
+  );
+
+  const dispatch = useDispatch();
+  useProductFilter();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  return (
+    <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
+      <Filter categories={categories ? categories : []} />
+      {isLoading ? (
+        <Loader />
+      ) : errorMessage ? (
+        <div className="flex justify-center items-center h-[200px]">
+          <FaExclamationTriangle className="text-slate-800 text-3xl mr-2" />
+          <span className="text-slate-800 text-l font-medium">
+            {errorMessage}
+          </span>
+        </div>
+      ) : (
+        <div className="min-h-[700]">
+          <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+            {products &&
+              products.map((item, i) => <ProductCard key={i} {...item} />)}
+          </div>
+          <div className="flex justify-center pt-10">
+            <PaginationForProduct 
+            numberOfPage={pagination?.totalPages}
+            totalProducts={pagination?.totalElements}
+             />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Products;
---
+++ dhakastore/chaubisedhakaFrontend/src/components/shared/ProductCard.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import ProductViewModal from "./ProductViewModal";
+import trauncateText from "../../utils/trauncateText";
+
+const ProductCard = ({
+  productId,
+  productName,
+  image,
+  description,
+  quantity,
+  price,
+  discount,
+  specialPrice,
+}) => {
+  const [openProductViewModal, setOpenProductViewModel] = useState(false);
+  const btnLoader = false;
+  const [selectedViewProduct, setSelectedViewProduct] = useState("");
+  const isAvailable = quantity && Number(quantity) > 0;
+
+  const handleProductView = (product) => {
+    setSelectedViewProduct(product);
+    setOpenProductViewModel(true);
+  };
+  return (
+    <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
+      <div
+        onClick={() => {
+          handleProductView({
+            id: productId,
+            productName,
+            image,
+            description,
+            quantity,
+            price,
+            discount,
+            specialPrice,
+          });
+        }}
+        className="w-full overflow-hidden aspect-3/2"
+      >
+        <img
+          className="h-full w-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
+          src={image}
+          alt={productName}
+        />
+      </div>
+      <div className="p-4">
+        <h2
+          onClick={() =>
+            handleProductView({
+              id: productId,
+              productName,
+              image,
+              description,
+              quantity,
+              price,
+              discount,
+              specialPrice,
+            })
+          }
+          className="text-lg font-semibold mb-2 cursor-pointer"
+        >
+          {trauncateText(productName, 50)}
+        </h2>
+        <div className="min-h-20 max-h-20">
+          <p className="text-gray-600 text-sm">
+            {trauncateText(description, 80)}
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          {specialPrice ? (
+            <div className="flex flex-col">
+              <span className="text-gray-400 line-through">
+                ${Number(price).toFixed(2)}
+              </span>
+              <span className="text-lg text-gray-700 ">
+                ${Number(specialPrice).toFixed(2)}
+              </span>
+            </div>
+          ) : (
+            <span className="text-lg text-gray-700 ">
+              {" "}
+              ${Number(price).toFixed(2)}
+            </span>
+          )}
+          <button
+            disabled={!isAvailable || btnLoader}
+            className={`bg-blue-500 ${
+              isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"
+            }
+            text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}
+          >
+            <FaShoppingCart className="mr-3" />
+            {isAvailable ? "Add to Cart" : "Stock Out"}
+          </button>
+        </div>
+      </div>
+      <ProductViewModal
+        open={openProductViewModal}
+        setOpen={setOpenProductViewModel}
+        product={selectedViewProduct}
+        isAvailable={isAvailable}
+      />
+    </div>
+  );
+};
+
+export default ProductCard;
