0c63cac8877e56780e0da3bdb857b2f69e7ec969
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-07 21:45:44 +0545

    displaying products under hero section

--- dhakastore/chaubisedhakaFrontend/src/components/home/Home.jsx
+++ dhakastore/chaubisedhakaFrontend/src/components/home/Home.jsx
@@ -1,8 +1,25 @@
-import HeroBanner from "../home/HeroBanner"
+import { useDispatch, useSelector } from "react-redux";
+import HeroBanner from "../home/HeroBanner";
+import { useEffect } from "react";
+import { fetchProducts } from "../../store/actions";
+import ProductCard from "../shared/ProductCard";
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
   return (
     <div>
-     <HeroBanner/>
+      <HeroBanner />
+      <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+        {products &&
+          products
+            ?.slice(0, 8)
+            .map((item, i) => <ProductCard key={i} {...item} />)}
+      </div>
     </div>
   );
 };
