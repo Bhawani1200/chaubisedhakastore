306d43f194b78bb34e81c241410214b7874abbe9
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-07 08:51:57 +0545

    slight changes

--- dhakastore/chaubisedhakaFrontend/src/components/home/HeroBanner.jsx
+++ dhakastore/chaubisedhakaFrontend/src/components/home/HeroBanner.jsx
@@ -1,13 +1,10 @@
-import { Pagination } from "@mui/material";
-import { Autoplay, EffectFade, Navigation } from "swiper/modules";
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
 import { bannerList } from "../../utils";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-const colors = [
-  "bg-color-[#FDC200]",
-  "bg-color-[#FF2C2C]",
-  "bg-color-[#21AD61]",
-];
+const colors = ["bg-[#FDC200]", "bg-[#FF2C2C]", "bg-[#21AD61]"];
+
 const HeroBanner = () => {
   return (
     <div className="py-2 rounded-md">
@@ -23,19 +20,22 @@ const HeroBanner = () => {
         scrollbar={{ draggable: true }}
         slidesPerView={1}
       >
-        {bannerList.map((item, i) => {
-          <SwiperSlide>
+        {bannerList.map((item, i) =>(
+             <SwiperSlide key={item.id}>
             <div
               className={`carousel-item rounded-md sm:h-[500px] h-96 ${colors[i]}`}
             >
               <div className="flex justify-center items-center">
                 <div className="text-center">
-                  <h3>{item.title}</h3>
+                  <h3 className="text-3xl text-white font-bold">
+                    {item.title}
+                  </h3>
+                  <h1 className="text-5xl text-white font-bold mt-2">{item.subtitle}</h1>
                 </div>
               </div>
             </div>
-          </SwiperSlide>;
-        })}
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
