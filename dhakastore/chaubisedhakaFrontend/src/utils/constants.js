45bfabd85ba61d1be80f95f706a98f7b688e50ac
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-07 21:23:58 +0545

    new images

---
+++ dhakastore/chaubisedhakaFrontend/src/utils/constants.js
@@ -0,0 +1,8 @@
+import image1 from "../assets/sliders/image1.jpg"
+import image2 from "../assets/sliders/image2.jpg"
+import image3 from "../assets/sliders/image3.jpg"
+export {
+    image1,
+    image2,
+    image3
+}
--- dhakastore/chaubisedhakaFrontend/src/utils/index.js
+++ dhakastore/chaubisedhakaFrontend/src/utils/index.js
@@ -1,8 +1,10 @@
+import { image1, image2, image3 } from "./constants";
+
 export const bannerList = [
   {
     id: 1,
     image:
-      "https://scontent.fbir1-1.fna.fbcdn.net/v/t39.30808-6/561897869_1143245321347199_6857312474468749621_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=f727a1&_nc_ohc=OKSEEI0KK5wQ7kNvwFj9B5C&_nc_oc=AdmTvnsVK7iW35dFyy_p6b99T4Dx-SKHEDbnq6lz9ZNp6W4is70n8K25i23PVyk70jtmURK9o0SSUTNq3-VLi1i-&_nc_zt=23&_nc_ht=scontent.fbir1-1.fna&_nc_gid=BbQlHGJ4JY2yknZjvsTGwQ&oh=00_Afh6CkD5gtFSlaOvBFfyRFpqcUhi0Mdjy6GyN8mnrX98cA&oe=69127AEA",
+     image1,
     title: "Timeless Elegance",
     subtitle: "Discover the beauty of traditional Nepali sarees",
     description:
@@ -11,7 +13,7 @@ export const bannerList = [
   {
     id: 2,
     image:
-      "https://scontent.fbir1-1.fna.fbcdn.net/v/t39.30808-6/558097924_1132458075759257_2503131407101069892_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=108&ccb=1-7&_nc_sid=f727a1&_nc_ohc=v0p0kyc9f8sQ7kNvwEmiMXc&_nc_oc=AdmMRBiHJ2rqBgvyfEZ3eKaHP7msGIpn-tBcOgwwCq-YfTnVtvrQSuqObXUqI2gxXVf7cNVSYxjl80KxAxFboHT3&_nc_zt=23&_nc_ht=scontent.fbir1-1.fna&_nc_gid=6ZWyVlqjzpNKsOw5LEdY9A&oh=00_AfjS56_PqFnyXsEa6Jlot9JfdfUztk0wvTGhWUId3SPl4g&oe=69129DF2",
+      image2,
     title: "Heritage Collection",
     subtitle: "Authentic traditional wear for women",
     description:
@@ -20,7 +22,7 @@ export const bannerList = [
   {
     id: 3,
     image:
-      "https://scontent.fbir1-1.fna.fbcdn.net/v/t39.30808-6/482031631_972566405081759_726072580753594994_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=dugF2EgYgRkQ7kNvwFuBSQv&_nc_oc=AdmPgQxL_GqU6SjlFBGJQ5o2S796EwY5dM5KAPFSfduh0Mamse-nJP9jmthlK9maoSrzeFqho3zg50LmsKdcrbZy&_nc_zt=23&_nc_ht=scontent.fbir1-1.fna&_nc_gid=NYfn9-q8enT2drHHvKWVGg&oh=00_AfjOU0IsrN3XHXnFnesLPkFxyb5UvjSjhhcTcRP66O2nAg&oe=6912A5A7",
+      image3,
     title: "Festive Splendor",
     subtitle: "Celebrate in style with our festive collection",
     description: "Perfect for weddings, festivals, and special occasions",
