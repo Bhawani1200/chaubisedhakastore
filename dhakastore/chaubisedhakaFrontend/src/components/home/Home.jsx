a0a166a5634510d3f60a7eefb63e5238a7c46014
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-11-08 15:09:13 +0545

    setting up home page

--- dhakastore/chaubisedhakaFrontend/src/components/home/Home.jsx
+++ dhakastore/chaubisedhakaFrontend/src/components/home/Home.jsx
@@ -3,22 +3,52 @@ import HeroBanner from "../home/HeroBanner";
 import { useEffect } from "react";
 import { fetchProducts } from "../../store/actions";
 import ProductCard from "../shared/ProductCard";
+import Loader from "../shared/Loader";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const Home = () => {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
 
+  const { isLoading, errorMessage } = useSelector((state) => state.errors);
+
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
   return (
-    <div>
-      <HeroBanner />
-      <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
-        {products &&
-          products
-            ?.slice(0, 8)
-            .map((item, i) => <ProductCard key={i} {...item} />)}
+    <div className="lg:px-14 sm:px-8 px-4">
+      <div className="py-6">
+        <HeroBanner />
+      </div>
+      <div className="py-5">
+        <div className="flex flex-col justify-center items-center space-y-2">
+          <h1 className="mb-4 font-bold text-3xl md:text-4xl text-balance text-foreground">
+            Our Products
+          </h1>
+          <p className="text-2xl text-muted-foreground font-semibold  mx-auto text-pretty text-gray-700">
+            {" "}
+            Explore our extensive collection of authentic traditional clothing
+          </p>
+        </div>
+        {isLoading ? (
+          <Loader />
+        ) : errorMessage ? (
+          HeroBanner(
+            <div className="flex justify-center items-center h-[200px]">
+              <FaExclamationTriangle className="text-slate-800 text-3xl mr-2" />
+              <span className="text-slate-800 text-l font-medium">
+                {errorMessage}
+              </span>
+            </div>
+          )
+        ) : (
+          <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+            {products &&
+              products
+                ?.slice(0, 8)
+                .map((item, i) => <ProductCard key={i} {...item} />)}
+          </div>
+        )}
       </div>
     </div>
   );
