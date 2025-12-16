8798e7be5dfd4686499cd6266919907d511a92b7
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-12-16 22:18:01 +0545

    missing piece

--- dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/repositories/CartItemRepository.java
+++ dhakastore/chaubisedhakaBackend/src/main/java/Backend/chaubisedhakaBackend/repositories/CartItemRepository.java
@@ -1,12 +1,9 @@
 package Backend.chaubisedhakaBackend.repositories;
 
-import Backend.chaubisedhakaBackend.model.Cart;
 import Backend.chaubisedhakaBackend.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
 
@@ -17,7 +14,8 @@ public interface CartItemRepository extends JpaRepository<CartItem,Long> {
     @Query("DELETE FROM CartItem ci WHERE ci.cart.id=?1 AND ci.product.id=?2 ")
     void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 
+    @Modifying
     @Query("DELETE FROM CartItem ci WHERE ci.cart.id=?1 ")
-    List<Cart> deleteAllByCardId(Long cartId);
+    void deleteAllByCardId(Long cartId);
 
 }
