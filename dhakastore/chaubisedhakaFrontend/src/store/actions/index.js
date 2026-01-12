99c1e1827d572d0cc8bc1f731898ea5895d4a8cd
Bhawani1200 <bhawanilimbu1200@gmail.com>
2026-01-12 22:21:39 +0545

    implementing category handler for admin

--- dhakastore/chaubisedhakaFrontend/src/store/actions/index.js
+++ dhakastore/chaubisedhakaFrontend/src/store/actions/index.js
@@ -501,3 +501,53 @@ export const addNewProductFromDashboard =
       setLoader(false);
     }
   };
+
+export const createCategoryDashboardAction =
+  (sendData, setOpen, reset, toast) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: "CATEGORY_LOADER" });
+      await api.post("/admin/categories", sendData);
+      dispatch({ type: "CATEGORY_SUCCESS" });
+      reset();
+      toast.success("Category Created Successful");
+      setOpen(false);
+      await dispatch(getAllCategoriesDashboard());
+    } catch (err) {
+      console.log(err);
+      toast.error(
+        err?.response?.data?.categoryName || "Failed to create new category"
+      );
+
+      dispatch({
+        type: "IS_ERROR",
+        payload: err?.response?.data?.message || "Internal Server Error",
+      });
+    }
+  };
+
+export const updateCategoryDashboardAction =
+  (sendData, setOpen, categoryID, reset, toast) =>
+  async (dispatch, getState) => {
+    try {
+      dispatch({ type: "CATEGORY_LOADER" });
+
+      await api.put(`/admin/categories/${categoryID}`, sendData);
+
+      dispatch({ type: "CATEGORY_SUCCESS" });
+
+      reset();
+      toast.success("Category Update Successful");
+      setOpen(false);
+      await dispatch(getAllCategoriesDashboard());
+    } catch (err) {
+      console.log(err);
+      toast.error(
+        err?.response?.data?.categoryName || "Failed to update category"
+      );
+
+      dispatch({
+        type: "IS_ERROR",
+        payload: err?.response?.data?.message || "Internal Server Error",
+      });
+    }
+  };
